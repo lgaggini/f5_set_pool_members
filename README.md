218f2dfa64eda73fa3c06b7f15e4883fd6eb6a57
Lorenzo Gaggini <lg@lgaggini.net>
2019-05-30 16:21:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# f5_set_pool_members
+
+python script to change the status of a pool members on F5 using the [F5 iControl® REST Python API](https://github.com/F5Networks/f5-common-python)
+
+## Install
+### Clone
+```bash
+git clone git@git.dadapro.net:lgaggini/spartacus.git
+```
+
+### Requirements
+```bash
+pip install -r requirements.txt
+```
+## Configuration
+Configure your credentials and your endpoint in a settings file which will be included.
+```python
+ENDPOINT='my.endpoint.it'
+USER='myuser'
+PASS='mypass'
+```
+
+## Usage
+
+```bash
+usage: f5_set_pool_members.py [-h] -p {popper-stag-priv-http} -s
+                              {enabled,disabled,forced_offline} [-r]
+
+enables, disables and forces offline f5 pools members
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p {popper-stag-priv-http}, --pool {popper-stag-priv-http}
+                        pool to work with
+  -s {enabled,disabled,forced_offline}, --state {enabled,disabled,forced_offline}
+                        pool to work with
+  -r, --readonly        readonly mode for debug (default disabled)
+```
---
+++ f5_set_pool_members.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+from f5.bigip import ManagementRoot
+import argparse
+import logging
+from settings import ENDPOINT, USER, PASS
+
+available_pools = ['popper-stag-priv-http']
+available_states = ['enabled', 'disabled', 'forced_offline']
+
+description = 'enables, disables and forces offline f5 pools members'
+parser = argparse.ArgumentParser(description=description)
+
+parser.add_argument('-p', '--pool', required=True, choices=available_pools,
+                    help='pool to work with')
+parser.add_argument('-s', '--state', required=True, choices=available_states,
+                    help='pool to work with')
+parser.add_argument('-r', '--readonly', dest='readonly',
+                    action='store_true',
+                    help='readonly mode for debug (default disabled)')
+
+cli_options = parser.parse_args()
+FORMAT = '%(asctime)s %(levelname)s %(module)s %(message)s'
+logging.basicConfig(format=FORMAT, level='INFO')
+logger = logging.getLogger('set_pool_members_state')
+
+mgmt = ManagementRoot(ENDPOINT, USER, PASS)
+
+pool = mgmt.tm.ltm.pools.pool.load(name=cli_options.pool,
+                                   partition='Common')
+
+for member in pool.members_s.get_collection():
+
+    logger.info('%s: %s %s' % (member.name, member.session, member.state))
+
+    if cli_options.state == 'enabled':
+        # enables member
+        logger.info('enables member %s, previous state: %s' %
+                    (member.name, member.state))
+        member.session = 'user-enabled'
+    elif cli_options.state == 'disabled':
+        # disables member
+        logger.info('disables member %s, previous state: %s' %
+                    (member.name, member.state))
+        member.session = 'user-disabled'
+    elif cli_options.state == 'forced_offline':
+        # forces online member
+        logger.info('forces online member %s, previous state: %s' %
+                    (member.name, member.state))
+        member.state = 'user-down'
+        member.session = 'user-disabled'
+
+    if not cli_options.readonly:
+        member.update()
+    else:
+        logger.info('readonly mode, no changes applied')
+
+    logger.info('%s: %s %s' % (member.name, member.session, member.state))
